937cef07f2ced4872295d596031a76918accc2d4
gnath-aswin <aswingnath19@gmail.com>
2025-10-08 21:35:11 +0200

    Add variables.tf

--- terrademo/main.tf
+++ terrademo/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "ny-rides-aswin"
-  region  = "europe-west1-b"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "ny-rides-aswin-terra-bucket-916"
-  location      = "EU"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
